602d5543d1215f1452791d95f8356e63dc347d87
samarth paboowal <paboowal@samarths-Mac.local>
2015-04-03 21:24:31 +0530

    Creating project

---
+++ Change My Voice/Change My Voice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Change My Voice
+//
+//  Created by samarth paboowal on 4/3/15.
+//  Copyright (c) 2015 samarth paboowal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Change My Voice/Change My VoiceTests/Change_My_VoiceTests.swift
@@ -0,0 +1,36 @@
+//
+//  Change_My_VoiceTests.swift
+//  Change My VoiceTests
+//
+//  Created by samarth paboowal on 4/3/15.
+//  Copyright (c) 2015 samarth paboowal. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Change_My_VoiceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
