f194cf4151baf99832e3d102010c01f4dfe48d94
Samarth Paboowal <samarthpaboowal@yahoo.co.in>
2015-04-03 23:17:41 +0530

    Add recording button with action

--- Change My Voice/Change My Voice/ViewController.swift
+++ Change My Voice/Change My Voice/ViewController.swift
@@ -20,6 +20,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        // TODO: Show text "recording in progress"
+        // TODO: Record user's voice
+        println("in recordAudio")
+    }
 
 }
 
