100c4e1d648b8f5cc473d57505a9158f562b6918
samarth paboowal <paboowal@samarths-Mac.local>
2015-04-04 20:48:34 +0530

    Add Navigation Controller and stop button and labels on the main screen

--- Change My Voice/Change My Voice/ViewController.swift
+++ Change My Voice/Change My Voice/ViewController.swift
@@ -10,18 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var recordingInProgress: UILabel!
 
+    @IBOutlet weak var labelStopRecording: UILabel!
+    
+    @IBOutlet weak var stopRecording: UIButton!
+    
+    @IBOutlet weak var recordAudio: UIButton!
+    
+    @IBAction func stopRecording(sender: UIButton) {
+        recordingInProgress.hidden = true
+        labelStopRecording.hidden = true
+        recordAudio.enabled = true
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        // Hide the stop button initially
+        stopRecording.hidden = true
+        recordAudio.enabled = true
+    }
 
     @IBAction func recordAudio(sender: UIButton) {
-        // TODO: Show text "recording in progress"
+        recordingInProgress.hidden = false
+        labelStopRecording.hidden = false
+        stopRecording.hidden = false
+        recordAudio.enabled = false
+        
         // TODO: Record user's voice
         println("in recordAudio")
     }
